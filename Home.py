85b0a8c23a48c4891c720ea7562b8a9c193ee376
Steven <stevenjmunn@gmail.com>
2023-03-20 23:50:35 -0700

    add links

--- Home.py
+++ Home.py
@@ -1,14 +1,28 @@
 import streamlit as st
 
-st.title("Predictive Maintenance Tool")
+st.title("Predictive Maintenance Tool for Azure Computing Data")
 
 st.write("Steven Munn, March 20th 2023")
 
-st.write("Using the Kaggle data-set for \
-[Azure Predictive Maintenance Challenge](https://www.kaggle.com/datasets/arnabbiswas1/microsoft-azure-predictive-maintenance) \
+st.markdown(
+"""
+Tabs on the left allow you to:
+- Infer likely failure time-intervals for components with the [Predictions Day Model](/Predictions_Day_Model) or [Predictions Two Day Model](/Predictions_Two_Day_Model).
+- Visualize pre-failure data in the [Equipment Failure Visualizations](/Equipment_Failure_Visualizations) tab.
+- Examine training reports in the [Reports](/Reports) tab.
+
+The sections below give an overview of the predictive maintenance problem.
+"""
+)
+
+
+with st.expander("Problem Description"):
+
+    st.write("Using the Kaggle data-set for [Azure Predictive Maintenance Challenge](https://www.kaggle.com/datasets/arnabbiswas1/microsoft-azure-predictive-maintenance) \
         this project explores ways to model and predict failures for component 2.")
 
-with st.expander("Problem Overview"):
+with st.expander("Data Description"):
+
     st.image("img/task.png")
     st.write("This project aims to train predictive models based on historical data from the APM challenge dataset.")
 
@@ -26,7 +40,7 @@ with st.expander("Problem Overview"):
 
 
 with st.expander("Predictive Models"):
-    st.write("See model predictions in the Day Model Predictions or Two Day Model Predictions tabs.")
+    st.write("After splitting the data from the Kaggle dataset into training, validation, and testing data, the predictive models will use testing data (unseen by the models) to compare predictions to ground truth.")
 
 with st.expander("Visualizations"):
-    st.write("Inspect data before a failure in the Equipment Failure Visualizations tab.")
+    st.write("The visualization tab helps the user plot variables leading up to a certain time to see patterns in the failures.")
